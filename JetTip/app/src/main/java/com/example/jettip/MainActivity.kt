184b407ce2317d3117b9646c2e2daff2c516b419
jongukjeon <jongukjeon@confitech.co.kr>
2024-04-16 22:52:07 +0900

    JetTipApp 위치 정렬, 커스텀 위젯

--- JetTip/app/src/main/java/com/example/jettip/MainActivity.kt
+++ JetTip/app/src/main/java/com/example/jettip/MainActivity.kt
@@ -9,15 +9,22 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Remove
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Slider
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -33,10 +40,12 @@ import androidx.compose.ui.platform.SoftwareKeyboardController
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.jettip.components.InputField
 import com.example.jettip.ui.theme.JetTipTheme
+import com.example.jettip.widgets.RoundIconButton
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -68,6 +77,7 @@ fun TopHeader(totalPerPerson: Double = 134.0) {
     Surface(
         modifier = Modifier
             .fillMaxWidth()
+            .padding(15.dp)
             .height(150.dp)
 //            .clip(shape = RoundedCornerShape(corner = CornerSize(12.dp)))
             .shadow(10.dp, shape = RoundedCornerShape(corner = CornerSize(12.dp))),
@@ -109,23 +119,34 @@ fun BillForm(
 ) {
     // 현재 입력 값
     val totalBillState = remember {
-        mutableStateOf("")
+        mutableStateOf("0")
     }
+
     // 입력 값이 Empty인지 확인
     val validState: Boolean = remember(totalBillState.value) {
         totalBillState.value.trim().isNotEmpty()
     }
+
     // 키보드 컨트롤
     val keyboardController: SoftwareKeyboardController? = LocalSoftwareKeyboardController.current
+
+    // 슬라이더 값
+    val sliderPositionState = remember {
+        mutableStateOf(0f)
+    }
 
     Surface(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(2.dp),
+            .padding(15.dp),
         shape = RoundedCornerShape(corner = CornerSize(8.dp)),
         border = BorderStroke(width = 2.dp, color = colorResource(id = R.color.FFD5D5D5))
     ) {
-        Column {
+        Column(
+            modifier = Modifier.padding(6.dp),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.Start
+        ) {
             InputField(
                 valueState = totalBillState,
                 labelId = "Enter Bill",
@@ -133,13 +154,108 @@ fun BillForm(
                 isSingleLine = true,
                 onAction = KeyboardActions {
                     // Enter 눌렀을 경우
-                    if(!validState) return@KeyboardActions
+                    if (!validState) {
+                        Log.d("MYTAG", "값 없음")
+                        return@KeyboardActions
+                    }
 
                     onValChange(totalBillState.value.trim())
 
                     keyboardController?.hide()
                 }
             )
+            if (validState) {
+                // Split
+                Row(
+                    modifier = Modifier
+                        .padding(10.dp),
+                    horizontalArrangement = Arrangement.Start
+                ) {
+                    Text(
+                        text = "Split",
+                        modifier = Modifier
+                            .align(alignment = Alignment.CenterVertically)
+                    )
+//                    Spacer(modifier = Modifier.width(120.dp))
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        horizontalArrangement = Arrangement.End
+                    ) {
+                        RoundIconButton(
+                            imageVector = Icons.Default.Remove,
+                            onClick = {
+                                Log.d("MYTAG", "Minus Clicked")
+                            }
+                        )
+
+                        Text(
+                            modifier = Modifier
+                                .align(Alignment.CenterVertically)
+                                .padding(horizontal = 9.dp),
+                            text = "2"
+                        )
+
+                        RoundIconButton(
+                            imageVector = Icons.Default.Add,
+                            onClick = {
+                                Log.d("MYTAG", "Plus Clicked")
+                            }
+                        )
+                    }
+                }
+
+                // Tip Row
+                Row(
+                    modifier = Modifier
+                        .padding(10.dp)
+                ) {
+                    Text(
+                        modifier = Modifier
+                            .align(alignment = Alignment.CenterVertically),
+                        text = "Tip"
+                    )
+//                    Spacer(modifier = Modifier.foldIn().width(200.dp))
+                    Text(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 6.dp),
+                        textAlign = TextAlign.End,
+                        text = "$33.00"
+                    )
+                }
+
+                // Percentage
+                Column(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = "%.0f".format(sliderPositionState.value * 100) + "%"
+                    )
+
+                    Spacer(modifier = Modifier.height(14.dp))
+
+                    // Slider
+                    Slider(
+                        value = sliderPositionState.value,
+                        onValueChange = {
+                            sliderPositionState.value = it
+                            Log.d("MYTAG", "${it * 100}")
+                        },
+                        modifier = Modifier.padding(horizontal = 16.dp),
+                        steps = 5,
+                        onValueChangeFinished = {
+                            Log.d("MYTAG", "onValueChangeFinished()")
+                        }
+                    )
+                }
+            } else {
+                Box {
+
+                }
+            }
         }
     }
 }
