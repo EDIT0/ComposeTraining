69ab9c9c83418028e7efe0ff1ef59b13f3d00bf9
jongukjeon <jongukjeon@confitech.co.kr>
2024-05-19 16:17:23 +0900

    Revert "JetTriviaApp Data return form, Repository, ViewModel, 문제, 선택 버튼, 다음 버튼 UI 추가"
    This reverts commit 635bcbd08ce60103b31dbab6585e653e71f581e0.

--- JetTriviaApp/app/src/main/java/com/example/jettriviaapp/screens/TriviaHome.kt
+++
@@ -1,9 +0,0 @@
-package com.example.jettriviaapp.screens
-
-import androidx.compose.runtime.Composable
-import com.example.jettriviaapp.component.Questions
-
-@Composable
-fun TriviaHome(questionsViewModel: QuestionsViewModel) {
-    Questions(questionsViewModel)
-}
