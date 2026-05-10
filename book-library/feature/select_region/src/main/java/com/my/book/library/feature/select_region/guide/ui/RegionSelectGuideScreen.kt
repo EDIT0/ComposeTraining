6c427373eb03a86199d7d207be974f26ae7373b6
JongukJeon <akdmadl34@naver.com>
2026-05-10 16:58:01 +0900

    book-library feat: 디자인 요소 추가  - 폰트  - 지역 등록 안내 UI 디자인

--- book-library/feature/select_region/src/main/java/com/my/book/library/feature/select_region/guide/ui/RegionSelectGuideScreen.kt
+++ book-library/feature/select_region/src/main/java/com/my/book/library/feature/select_region/guide/ui/RegionSelectGuideScreen.kt
@@ -2,7 +2,6 @@ package com.my.book.library.feature.select_region.guide.ui
 
 import android.content.Context
 import androidx.activity.compose.BackHandler
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.WindowInsets
@@ -12,31 +11,35 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBars
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.material3.Button
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.compose.LocalLifecycleOwner
 import com.my.book.library.core.common.CommonViewModel
-import com.my.book.library.core.common.component.CommonActionBar
+import com.my.book.library.core.common.component.CommonButton
 import com.my.book.library.core.common.component.LifecycleListener
 import com.my.book.library.core.common.component.LifecycleResult
-import com.my.book.library.core.common.noRippleClickable
+import com.my.book.library.core.common.dpToSp
 import com.my.book.library.core.common.util.LogUtil
 import com.my.book.library.core.common.util.SystemBarConfig
 import com.my.book.library.core.common.util.SystemBarController
-import com.my.book.library.core.resource.Black
-import com.my.book.library.core.resource.LibraryData
+import com.my.book.library.core.resource.NotoSansKR
 import com.my.book.library.core.resource.R
 import com.my.book.library.feature.select_region.guide.intent.RegionSelectGuideViewModelEvent
 import com.my.book.library.feature.select_region.guide.viewmodel.RegionSelectGuideViewModel
@@ -105,19 +108,22 @@ fun RegionSelectGuideContent(
     regionSelectGuideViewModelEvent: (RegionSelectGuideViewModelEvent) -> Unit
 ) {
 
+    val useStatusBarSpace = false
+    val useNavigationBarSpace = true
+
     SystemBarController.Setup(
         config = SystemBarConfig(
-            statusBarColor = Black,
-            statusBarDarkIcons = false,
-            useStatusBarSpace = true,
-            navigationBarColor = Black,
-            navigationBarDarkIcons = false,
-            useNavigationBarSpace = true
+            statusBarColor = colorResource(R.color.color_FFFFFFFF),
+            statusBarDarkIcons = true,
+            useStatusBarSpace = useStatusBarSpace,
+            navigationBarColor = colorResource(R.color.color_FFFFFFFF),
+            navigationBarDarkIcons = true,
+            useNavigationBarSpace = useNavigationBarSpace
         )
     ) { state ->
         Scaffold(
             modifier = Modifier
-                .padding(top = state.statusBarHeight, bottom = state.navigationBarHeight)
+                .padding(top = if(useStatusBarSpace) {state.statusBarHeight} else {0.dp}, bottom = if(useNavigationBarSpace) {state.navigationBarHeight} else {0.dp})
                 .consumeWindowInsets(WindowInsets.statusBars)
                 .consumeWindowInsets(WindowInsets.navigationBars)
         ) { innerPadding ->
@@ -128,33 +134,62 @@ fun RegionSelectGuideContent(
                 Column(
                     modifier = Modifier
                         .fillMaxSize()
+                        .background(color = colorResource(R.color.color_FFFFFFFF))
                 ) {
-                    CommonActionBar(
-                        context = localContext,
-                        modifier = Modifier
-                            .padding(horizontal = 20.dp),
-                        actionBarTitle = "지역선택안내 액션바",
-                        isShowBackButton = false,
-                        onBackClick = {
-                            onBackPressed.invoke()
-                        }
-                    )
-
                     Column(
                         modifier = Modifier
                             .fillMaxWidth()
                             .weight(1f)
+                            .padding(horizontal = 20.dp)
                     ) {
-                        Text(text = "이미지")
-                        Text(text = "어느 지역에서\n책을 찾을까요?")
-                        Text(text = "시도 -> 시군구 -> 순으로 선택하면\n해당 지역의 도서관 소장 정보를 보여드려요.")
-                        Button(
+                        Image(
+                            painter = painterResource(id = R.drawable.ic_point_marker_blue),
+                            contentDescription = null,
+                            modifier = Modifier
+                                .padding(top = 44.dp + if(useNavigationBarSpace) {state.navigationBarHeight} else {0.dp})
+                        )
+                        Spacer(modifier = Modifier.height(5.dp))
+                        Text(
+                            text = stringResource(R.string.select_region_title),
+                            style = TextStyle(
+                                color = colorResource(R.color.color_191F28),
+                                fontSize = dpToSp(26.dp),
+                                lineHeight = dpToSp(35.dp),
+                                fontFamily = NotoSansKR,
+                                fontWeight = FontWeight.Medium
+                            )
+                        )
+                        Spacer(modifier = Modifier.height(10.dp))
+                        Text(
+                            text = stringResource(R.string.select_region_subtitle),
+                            style = TextStyle(
+                                color = colorResource(R.color.color_6B7684),
+                                fontSize = dpToSp(16.dp),
+                                lineHeight = dpToSp(24.dp),
+                                fontFamily = NotoSansKR,
+                                fontWeight = FontWeight.Medium
+                            )
+                        )
+                    }
+                    Column(
+                        modifier = Modifier
+                            .wrapContentHeight()
+                    ) {
+                        CommonButton(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(start = 20.dp, top = 16.dp, end = 20.dp, bottom = 24.dp),
+                            text = stringResource(R.string.select_region_next_button_title),
+                            textColorRes = R.color.color_FFFFFFFF,
+                            backgroundColorRes = R.color.color_3182F6,
+                            cornerRadius = 12.dp,
+                            isBorderEnabled = false,
+                            isEnabled = true,
+                            textSize = 16.dp,
                             onClick = {
                                 onMoveToRegionSelection.invoke()
                             }
-                        ) {
-                            Text(text = "지역 선택 시작")
-                        }
+                        )
                     }
                 }
             }
